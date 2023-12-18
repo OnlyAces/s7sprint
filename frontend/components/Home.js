130385d376e6d1af7c1915c2b1750d0d194caab8
Isaac Henry <isaachenry751@gmail.com>
2023-12-17 21:05:11 -0500

    commit

---
+++ frontend/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import pizza from './images/pizza.jpg';
+import { Link } from 'react-router-dom';
+
+
+function Home() {
+ 
+
+  return (
+    <div>
+      <h2>Welcome to Bloom Pizza!</h2>
+      <Link to="/order">
+        <img alt="order-pizza" style={{ cursor: 'pointer' }} src={pizza} />
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
